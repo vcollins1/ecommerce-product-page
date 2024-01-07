85c2088787319d09c143e3d136509b1b92a34c05
vcollins1 <vontrellecollins@gmail.com>
2024-01-06 21:55:32 -0600

    image lightbox

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ const incDec = document.querySelectorAll(".inc-dec");
 const addCart = document.querySelector(".cart-btn");
 const cartCount = document.querySelector(".shopping-cart__count");
 const thumbnails = document.querySelectorAll(".thumbnail");
+const viewerSelector = document.querySelectorAll(".viewer__img-selector");
 let sneakerImg = 1;
 
 document.querySelector(".hambuger").addEventListener("click", e => {
@@ -15,20 +16,40 @@ document.querySelector(".close-icon").addEventListener("click", e => {
 
 document.querySelector(".next").addEventListener("click", e => {
     sneakerImg = (sneakerImg + 1) === 5 ? 1 : sneakerImg + 1;
-    document.querySelector(".slide-deck__img").src = `./assets/images/image-product-${sneakerImg}.jpg`
+    document.querySelector(".slide-deck__img").src = `./assets/images/image-product-${sneakerImg}.jpg`;
 })
 
 document.querySelector(".previous").addEventListener("click", e => {
     sneakerImg = (sneakerImg - 1) === 0 ? 4 : sneakerImg - 1;
-    document.querySelector(".slide-deck__img").src = `./assets/images/image-product-${sneakerImg}.jpg`
+    document.querySelector(".slide-deck__img").src = `./assets/images/image-product-${sneakerImg}.jpg`;
+})
+
+document.querySelector(".slide-deck__img").addEventListener("click", e => {
+    document.querySelector(".viewer-container").style.display = "flex";
+    document.querySelector(".viewer__img").src = `./assets/images/image-product-${sneakerImg}.jpg`;
+})
+
+document.querySelector(".viewer__close").addEventListener("click", e => {
+    document.querySelector(".viewer-container").style.display = "none";
 })
 
 thumbnails.forEach(thumbnail => {
     thumbnail.addEventListener("click", e => {
-        document.querySelector(".slide-deck__img").src = e.target.src.replace("-thumbnail", '');
-
         document.querySelector(".active-thumbnail").classList.remove("active-thumbnail");
         e.target.parentElement.classList.add("active-thumbnail");
+        document.querySelector(".slide-deck__img").src = e.target.src.replace("-thumbnail", '');
+    })
+})
+
+viewerSelector.forEach(btn => {
+    btn.addEventListener("click", e => {
+        if (e.target.classList.contains("viewer__img-selector--previous")) {
+            sneakerImg = (sneakerImg - 1) === 0 ? 4 : sneakerImg - 1;
+            document.querySelector(".viewer__img").src = `./assets/images/image-product-${sneakerImg}.jpg`;
+        } else {
+            sneakerImg = (sneakerImg + 1) === 5 ? 1 : sneakerImg + 1;
+            document.querySelector(".viewer__img").src = `./assets/images/image-product-${sneakerImg}.jpg`;
+        }
     })
 })
 
